2d3e2c8c6a6175d9e35a0597d7747015e053cc6b
Bilal Hassen <bilalhassen097@gmail.com>
2025-04-19 11:24:49 -0400

    add nav skeleton

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,13 @@
+
+export default Nav = () => {
+    return (
+        <>
+        <nav className="nav">
+            <h1 className="nav__logo">TriggerTech<span className="nav__logo-pink">Labs</span></h1>
+            <button className="nav__menu-btn">
+                
+            </button>
+        </nav>
+        </>
+    )
+}
