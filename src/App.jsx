0bcd3df202886cbe4fcbee0017b3dec95a35041d
Bilal Hassen <bilalhassen097@gmail.com>
2025-04-19 11:00:24 -0400

    add fonts and basic css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
-import './App.css'
+import './App.scss'
 
 function App() {
 
   return (
     <>
-    
+    <h1>Hi, Im Bilal Hassen</h1><br></br>
+    <p>and this is TriggerTech Labs <br></br>MotherFucker!</p>
     </>
   )
 }
