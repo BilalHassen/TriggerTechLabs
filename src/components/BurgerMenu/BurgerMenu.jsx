c949d9955b90ff00b02a0b4bd4d592432a38c4a9
Bilal Hassen <bilalhassen097@gmail.com>
2025-04-19 16:03:11 -0400

    create and add animations for interacting with the burger menu

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import "./BurgerMenu.scss";
 
 export const BurgerMenu = () => {
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = ()=>{
-        setIsOpen(!open)
-    }
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  
+
+
   return (
-    <button className="nav__burger-btn" onClick={toggleMenu}>
-    <span className="nav__burger-bar"></span>
-    <span className="nav__burger-bar"></span>
-    </button>
-  )
-}
+    <>
+    
+      <button className="nav__burger-btn" onClick={toggleMenu}>
+        <span className="nav__burger-bar"></span>
+        <span className="nav__burger-bar"></span>
+      </button>
+
+      <div className={`nav__menu ${isOpen ? "nav__menu-open" : ""}`}>
+        <h1>this is the menu</h1>
+      </div>
+
+      <div className={`nav__overlay ${isOpen ? "nav__overlay-open" : ""}`}></div>
+    </>
+  );
+};
