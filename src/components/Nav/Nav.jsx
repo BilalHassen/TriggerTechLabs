37a0aaed10f20eb9e385edbbe51de3921f40fe49
Bilal Hassen <bilalhassen097@gmail.com>
2025-04-20 12:35:26 -0400

    add header element

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,12 +3,14 @@ import { BurgerMenu } from "../BurgerMenu/BurgerMenu";
 const Nav = () => {
   return (
     <>
+    <header className="header">
       <nav className="nav">
         <h1 className="nav__logo">
           Trigger<span className="nav__logo-pink">Tech</span>
         </h1>
         <BurgerMenu/>
       </nav>
+      </header>
     </>
   );
 };
