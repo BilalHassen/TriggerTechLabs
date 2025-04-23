29ad40d2af49ac07aae42558f1abe3eded008871
Bilal Hassen <bilalhassen097@gmail.com>
2025-04-23 15:21:46 -0400

    create the NavMenuLarge folder and structure

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,6 +1,9 @@
 import "./Nav.scss";
 import { BurgerMenu } from "../BurgerMenu/BurgerMenu";
 import { useWidth } from "../../hooks/useWidth";
+import NavLargeMenu from "../NavLargeMenu/NavLargeMenu";
+
+
 const Nav = () => {
 
   const width = useWidth()
@@ -13,7 +16,7 @@ const Nav = () => {
         <h1 className="nav__logo">
           Trigger<span className="nav__logo-pink">Tech</span>
         </h1>
-        {width >= 768 ? null : <BurgerMenu/>}
+        {width >= 768 ? <NavLargeMenu/> : <BurgerMenu/>}
       </nav>
       </header>
     </>
