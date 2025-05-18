095ad3e99d3ddc6d95987acb265752a47e1dca0f
Bilal Hassen <bilalhassen097@gmail.com>
2025-05-18 14:29:35 -0400

    add images for tech

---
+++ src/Pages/Home/components/TechStack/TechStack.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TechStack() {
+  return (
+    <section className='tech-stack'>
+        <h2 className='tech-stack__title'>Tech Stack</h2>
+    </section>
+  )
+}
+
+export default TechStack
