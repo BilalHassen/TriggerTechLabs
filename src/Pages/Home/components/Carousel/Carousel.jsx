6c30ac41e4a1bdac545af25702157db7c25b4168
Bilal Hassen <bilalhassen097@gmail.com>
2025-05-18 13:47:30 -0400

    add items to carousel and fix spacing for the nav and base layout

--- src/Pages/Home/components/Carousel/Carousel.jsx
+++ src/Pages/Home/components/Carousel/Carousel.jsx
@@ -3,24 +3,29 @@ import "./Carousel.scss";
 
 const carouselItems = [
   "Web Developer",
-  "Front-End Development",
-  "AI Integration",
   "Custom Web Design",
+  "Mobile-First Design",
+  "Front-End Development",
+  "E-Commerce Development",
+  "API Integration",
+  "RESTful Services",
+  "AI-Powered Features",
   "Search Engine Optimization",
 ];
+
 
 function Carousel() {
   return (
     <div className="home__carousel">
-      <div className="carousel-track">
+      <div className="home__carousel-track">
         {carouselItems.map((item, index) => (
-          <div className="carousel-item" key={index}>
+          <div className="home__carousel-item" key={index}>
             {item}
           </div>
         ))}
         {/*duplication to make it look constant */}
         {carouselItems.map((item, index) => (
-      <div className="carousel-item" key={`dup-${index}`}>
+      <div className="home__carousel-item" key={`dup-${index}`}>
         {item}
       </div>
     ))}
