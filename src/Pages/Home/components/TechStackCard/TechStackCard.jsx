1e308837baf019ad89043bd2ff9ef242b2f1c981
Bilal Hassen <bilalhassen097@gmail.com>
2025-05-21 13:40:40 -0400

    styling for techstack section and increase size of nav logo

--- src/Pages/Home/components/TechStackCard/TechStackCard.jsx
+++ src/Pages/Home/components/TechStackCard/TechStackCard.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import "./TechStackCard.scss"
-function TechStackCard({techItem, name}) {
+
+function TechStackCard({ techItem, name }) {
   return (
     <div className='tech-stack__card'>
-    <svg className='tech-stack__icon'
-      width="60"
-      height="60"
-      viewBox="0 0 24 24"
-      fill={`#${techItem.icon.hex}`}
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path d={techItem.icon.path} />
-    </svg> 
-    <p className='tech-stack__name'>{name}</p>
+      <svg
+        className='tech-stack__icon'
+        viewBox="0 0 24 24"
+        fill={`#${techItem.icon.hex}`}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d={techItem.icon.path} />
+      </svg>
+      <p className='tech-stack__name'>{name}</p>
     </div>
-   )
-  
+  )
 }
 
 export default TechStackCard
