9d1643948b99fc3565570f2dd01c2a65af8c33e0
BilalHassen <bilalhassen097@gmail.com>
2025-07-30 01:10:08 -0400

    fix biography text

--- src/Pages/Home/components/MainSection/MainData.js
+++ src/Pages/Home/components/MainSection/MainData.js
@@ -4,6 +4,6 @@ export const JOB_TITLES = [
   "Web Developer",
 ];
 
-export const BIOGRAPHY = `At Trigger Tech Labs, I lead with one goal: building clean, powerful web experiences that actually make sense for your business. As a full-stack developer, I handle everything from front-end design to back-end logic — so you’re never stuck juggling freelancers or bloated agencies.
+export const BIOGRAPHY = `At Trigger Tech Labs, I focus on one mission: creating clean, powerful web experiences that drive real business results. As a full-stack developer, I handle everything from front-end design to back-end logic, so you never have to juggle multiple freelancers or deal with bloated agencies.
 
-  Whether you're launching a startup or leveling up your brand, I bring a sharp eye for detail, real-world problem solving, and a no-fluff approach to turning your vision into something fast, functional, and future-proof. This isn't just code — it's digital craftsmanship built to perform.`;
+Whether you're launching a startup or scaling your brand, I bring sharp attention to detail, practical problem-solving skills, and a streamlined approach to turning your vision into fast, functional, and future-proof solutions. This isn't just code, it's digital craftsmanship designed to perform.`;
